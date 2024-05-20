13603574f1d03af4c3dd9429866482bb934002cc
Jhonny Sierra <jhonnysierrap@gmail.com>
2024-05-20 16:30:56 -0500

    Run aplicación y application.yaml

---
+++ src/main/java/com/jhonny/coffee/webflux/postgresql/CoffeeWebfluxPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.jhonny.coffee.webflux.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeWebfluxPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeWebfluxPostgresqlApplication.class, args);
+	}
+
+}
