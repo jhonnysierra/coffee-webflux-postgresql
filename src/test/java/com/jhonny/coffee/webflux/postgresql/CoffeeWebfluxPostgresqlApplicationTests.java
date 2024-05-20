4da0e117c3899df52f29d888c951909270612c06
Jhonny Sierra <jhonnysierrap@gmail.com>
2024-05-20 16:32:49 -0500

    Create CoffeeWebfluxPostgresqlApplicationTests.java

---
+++ src/test/java/com/jhonny/coffee/webflux/postgresql/CoffeeWebfluxPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonny.coffee.webflux.postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeWebfluxPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
