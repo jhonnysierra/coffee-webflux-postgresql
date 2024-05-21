a1195ac7841018368f1db4c6cbf97d36d57bf11a
Jhonny Sierra <jhonnysierrap@gmail.com>
2024-05-21 17:12:42 -0500

    Se agregan métodos para mapear la clase completa incluyendo las relaciones con la tabla Country

--- src/main/java/com/jhonny/coffee/webflux/postgresql/service/mapper/CoffeeMapper.java
+++ src/main/java/com/jhonny/coffee/webflux/postgresql/service/mapper/CoffeeMapper.java
@@ -3,6 +3,7 @@ package com.jhonny.coffee.webflux.postgresql.service.mapper;
 
 import com.jhonny.coffee.webflux.postgresql.model.Coffee;
 import com.jhonny.coffee.webflux.postgresql.model.dto.CoffeeDTO;
+import com.jhonny.coffee.webflux.postgresql.model.dto.CoffeeDTORelation;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -10,8 +11,11 @@ import org.mapstruct.Mapping;
 public interface CoffeeMapper {
 
     @Mapping(target = "country", ignore = true)
-    Coffee convertirDTOACafe(CoffeeDTO coffeeDTO);
+    Coffee convertDTOToCoffee(CoffeeDTO coffeeDTO);
 
     CoffeeDTO convertCoffeeToDTO(Coffee coffee);
+    @Mapping(target = "originCountry", ignore = true)
+    Coffee convertDTOACafeAllFields(CoffeeDTORelation coffeeDTORelation);
 
+    CoffeeDTORelation convertDTOACafeAllFields(Coffee coffee);
 }
