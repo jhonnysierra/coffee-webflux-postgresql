fc0e8dc0d2080bf7f9c288337b240418162951e0
Jhonny Sierra <jhonnysierrap@gmail.com>
2024-05-21 17:11:53 -0500

    Se agrega métodos Handler para guardar, actualizar y consultar con la relación con la tabla Country

--- src/main/java/com/jhonny/coffee/webflux/postgresql/controller/CoffeeHandler.java
+++ src/main/java/com/jhonny/coffee/webflux/postgresql/controller/CoffeeHandler.java
@@ -1,9 +1,12 @@
 package com.jhonny.coffee.webflux.postgresql.controller;
 
 import com.jhonny.coffee.webflux.postgresql.model.dto.CoffeeDTO;
+import com.jhonny.coffee.webflux.postgresql.model.dto.CoffeeDTORelation;
 import com.jhonny.coffee.webflux.postgresql.service.ICoffeeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -16,16 +19,28 @@ import reactor.core.publisher.Mono;
 public class CoffeeHandler {
 
     private final ICoffeeService iCoffeeService;
-
+    private static final Logger logger = LoggerFactory.getLogger(CoffeeHandler.class);
     public Mono<ServerResponse> listenPOSTSaveCoffee(ServerRequest serverRequest) {
-        return null;
+
+        return serverRequest.bodyToMono(CoffeeDTO.class)
+                .flatMap(coffeeDTO ->
+                        ServerResponse
+                                .ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(iCoffeeService.saveCoffee(coffeeDTO), CoffeeDTO.class));
     }
 
     public Mono<ServerResponse> listenPATCHUpdateCoffee(ServerRequest serverRequest) {
-        return null;
+
+        return serverRequest.bodyToMono(CoffeeDTO.class)
+                .flatMap(coffeeDTO ->
+                        ServerResponse
+                                .ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(iCoffeeService.updateCoffee(coffeeDTO), CoffeeDTO.class));
     }
 
-    public Mono<ServerResponse> listenGETListAll(ServerRequest serverRequest) {
+    public Mono<ServerResponse> listenGETListAll() {
         return ServerResponse
                 .ok()
                 .contentType(MediaType.APPLICATION_JSON)
@@ -40,7 +55,16 @@ public class CoffeeHandler {
                 .body(iCoffeeService.findCoffeeById(id), CoffeeDTO.class);
     }
 
+    public Mono<ServerResponse> listenGETCoffeeByIdWithRelation(ServerRequest serverRequest) {
+        int id = Integer.parseInt(serverRequest.pathVariable("id"));
+        return ServerResponse
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(iCoffeeService.findCoffeeByIdWithRelation(id), CoffeeDTORelation.class);
+    }
+
     public Mono<ServerResponse> listenDELETEDeleteCoffee(ServerRequest serverRequest) {
         return null;
     }
+
 }
