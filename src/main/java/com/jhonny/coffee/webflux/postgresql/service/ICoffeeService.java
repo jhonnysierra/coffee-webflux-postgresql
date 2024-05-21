67fdc87c209b3e04f67ad5855e7a0d5f23f0e93a
Jhonny Sierra <jhonnysierrap@gmail.com>
2024-05-21 17:13:25 -0500

    Se agregan métodos de interfaz de servicio para almacenar y actualizar

--- src/main/java/com/jhonny/coffee/webflux/postgresql/service/ICoffeeService.java
+++ src/main/java/com/jhonny/coffee/webflux/postgresql/service/ICoffeeService.java
@@ -1,15 +1,19 @@
 package com.jhonny.coffee.webflux.postgresql.service;
 
-import com.jhonny.coffee.webflux.postgresql.model.Coffee;
 import com.jhonny.coffee.webflux.postgresql.model.dto.CoffeeDTO;
+import com.jhonny.coffee.webflux.postgresql.model.dto.CoffeeDTORelation;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ICoffeeService {
 
-    Mono<Coffee> saveCoffee(CoffeeDTO coffeeDTO);
+    Mono<CoffeeDTO> saveCoffee(CoffeeDTO coffeeDTO);
+
+    Mono<CoffeeDTO> updateCoffee(CoffeeDTO coffeeDTO);
 
     Mono<CoffeeDTO> findCoffeeById(int id);
+
+    Mono<CoffeeDTORelation> findCoffeeByIdWithRelation(int id);
 
     Flux<CoffeeDTO> listAllCoffee();
 
