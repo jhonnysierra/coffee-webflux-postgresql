b8abd3c6ae71813deec6f3709be6e882bf05248a
Jhonny Sierra <jhonnysierrap@gmail.com>
2024-05-23 15:05:20 -0500

    Update RouterConfig.java
    Se realiza configuración para documentación openapi

--- src/main/java/com/jhonny/coffee/webflux/postgresql/routerconfig/RouterConfig.java
+++ src/main/java/com/jhonny/coffee/webflux/postgresql/routerconfig/RouterConfig.java
@@ -2,15 +2,23 @@ package com.jhonny.coffee.webflux.postgresql.routerconfig;
 
 import com.jhonny.coffee.webflux.postgresql.controller.CoffeeHandler;
 import com.jhonny.coffee.webflux.postgresql.controller.CountryHandler;
+import com.jhonny.coffee.webflux.postgresql.service.ICoffeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
 
 /**
  * Clase que permite administrar las rutas de la aplicacion
@@ -26,14 +34,14 @@ public class RouterConfig {
 
     private final CountryHandler countryHandler;
 
-    /**
-     * Funcion que permite asignar la ruta a una funcion del controlador
-     *
-     * @return nido de rutas
-     */
     @Bean
+    @RouterOperations({
+            @RouterOperation(path = COFFEE + "/list/all", method = RequestMethod.GET, beanClass = ICoffeeService.class, beanMethod = "listAllCoffee"),
+            @RouterOperation(path = COFFEE + "/find/{id}", method = RequestMethod.GET, beanClass = ICoffeeService.class, beanMethod = "findCoffeeById",
+                    operation = @Operation(operationId = "findCoffeeById", summary = "Fetch coffee by Id", description = "Get Coffee info by id",
+                            parameters = {@Parameter(in = ParameterIn.PATH, name = "id")}))
+    })
     public RouterFunction<ServerResponse> routes() {
-
         return RouterFunctions.route(
                 POST(COFFEE + "/save").and(accept(MediaType.APPLICATION_JSON)),
                         coffeeHandler::listenPOSTSaveCoffee)
@@ -43,6 +51,11 @@ public class RouterConfig {
                         serverRequest -> coffeeHandler.listenGETListAll())
                 .andRoute(GET(COFFEE + "/find/{id}").and(accept(MediaType.APPLICATION_JSON)),
                         coffeeHandler::listenGETCoffeeById)
+                /*
+                  Para usar functional DSL se debe comenta la linea anterior y se habilita esta.
+                  Tambien se deberia eliminar la configuración por anotaciones
+                 */
+                /*.and(route().GET(COFFEE + "/find/{id}", coffeeHandler::listenGETCoffeeById, findCoffeeByIdOpenAPI()).build())*/
                 .andRoute(GET(COFFEE + "/find/relation/{id}").and(accept(MediaType.APPLICATION_JSON)),
                 coffeeHandler::listenGETCoffeeByIdWithRelation)
                 .andRoute(DELETE(COFFEE + "/delete/{id}").and(accept(MediaType.APPLICATION_JSON)),
@@ -50,6 +63,16 @@ public class RouterConfig {
                 .andRoute(GET(COUNTRY + "/list/all").and(accept(MediaType.APPLICATION_JSON)),
                         countryHandler::listenGETListAll)
                 ;
+
+
     }
+/*  private Consumer<Builder> findCoffeeByIdOpenAPI() {
+        return ops -> ops.tag("coffee")
+                .operationId("findCoffeeById").summary("Find purchase order by ID").tags(new String[] { "MyCoffee" })
+                .parameter(parameterBuilder().in(ParameterIn.PATH).name("id").description("Coffee Id"))
+                .response(responseBuilder().responseCode("200").description("successful operation").implementation(CoffeeDTO.class))
+                .response(responseBuilder().responseCode("400").description("Invalid Coffee ID supplied"))
+                .response(responseBuilder().responseCode("404").description("Coffee not found"));
+    }*/
 
 }
