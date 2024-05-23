5c6098baab1db64ea59de9a1ce983b97300a3106
Jhonny Sierra <jhonnysierrap@gmail.com>
2024-05-23 14:57:16 -0500

    Update CoffeeWebfluxPostgresqlApplication.java
    Se agregan anotaciones para definiciones openapi

--- src/main/java/com/jhonny/coffee/webflux/postgresql/CoffeeWebfluxPostgresqlApplication.java
+++ src/main/java/com/jhonny/coffee/webflux/postgresql/CoffeeWebfluxPostgresqlApplication.java
@@ -1,8 +1,11 @@
 package com.jhonny.coffee.webflux.postgresql;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Coffee Webflux Application" , version = "1.0" , description = "API de documentation v1.0"))
 @SpringBootApplication
 public class CoffeeWebfluxPostgresqlApplication {
 
